d1ce407fe40540ab361bd89780230f6792709004
g1fttt <george.goworow2015@gmail.com>
2024-04-30 18:12:10 +0200

    refactor: `wnd_proc.cpp` => `winapi.cpp`

--- src/hooks/winapi.cpp
+++ src/hooks/winapi.cpp
@@ -13,10 +13,10 @@ namespace winapi
   LRESULT WINAPI wnd_proc(HWND window, UINT message, WPARAM wparam,
                           LPARAM lparam) {
     if (ImGui_ImplWin32_WndProcHandler(window, message, wparam, lparam)) {
-      return true;
+      return TRUE;
     }
 
-    core::input.with(message, wparam, lparam, [&](const core::Input &input) {
+    core::input.with(message, wparam, lparam, [](const core::Input &input) {
       ui::menu.handle_toggle(input);
 
       if (input.key_is_up(VK_END)) {
