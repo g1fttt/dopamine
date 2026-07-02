52e0fba241dafe84a460347a00dd740ebb7e4200
saffage <90545232+saffage@users.noreply.github.com>
2026-07-02 23:41:20 +0300

    chore: remove `dopamine-macros` crate, replaced with declarative macros in `dopamine-sdk`

--- dopamine-macros/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod field_singleton_impl;
-
-use proc_macro::TokenStream;
-
-#[proc_macro_derive(FieldSingleton)]
-pub fn field_singleton(item: TokenStream) -> TokenStream {
-  field_singleton_impl::macro_impl(item)
-}
