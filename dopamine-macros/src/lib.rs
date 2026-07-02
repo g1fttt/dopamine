fd429f16c312003f1ca1a3431aa6d2a9270a13bc
saffage <90545232+saffage@users.noreply.github.com>
2026-07-02 23:03:16 +0300

    refactor: reimplement `netvar!` using `macro_rules!`

--- dopamine-macros/src/lib.rs
+++ dopamine-macros/src/lib.rs
@@ -1,12 +1,6 @@
 mod field_singleton_impl;
-mod netvar_impl;
 
 use proc_macro::TokenStream;
-
-#[proc_macro]
-pub fn netvar(item: TokenStream) -> TokenStream {
-  netvar_impl::macro_impl(item)
-}
 
 #[proc_macro_derive(FieldSingleton)]
 pub fn field_singleton(item: TokenStream) -> TokenStream {
